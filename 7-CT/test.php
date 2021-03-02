bfbc6467cf89bb0bf99dfe3a853e2a6cce8e7bb8
MEHBOOB RAZA <72504024+pasta29@users.noreply.github.com>
2021-03-02 18:23:47 +0530

    Add files via upload

---
+++ 7-CT/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload" name="submit">
+  <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="PLease mention the description of your file to be uploaded..">
+</textarea>
+</form>
+
+</body>
+</html>
